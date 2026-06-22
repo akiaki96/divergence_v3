5d72e2d23ee580942a17b32801291c16a93244a5
akiaki96 <akinabe1031@gmail.com>
2026-06-22 12:45:13 +0900

    init

--- Core/Src/app/app_main.cpp
+++ Core/Src/app/app_main.cpp
@@ -10,17 +10,23 @@ extern "C" {
 #include "gpio.h"
 }
 
-void app_main() {
+void app_init() {
     LOG("Hello divergence_v3!!\r\n");
-    HAL_TIM_Base_Start_IT(&htim6);
 
+    HAL_TIM_Base_Start_IT(&htim6);
     encoderLeft.init();
     encoderRight.init();
+}
+
+
+void app_main() {
+    app_init();
 
     uint16_t i = 0;
     while (true) {
         HAL_Delay(100);
-        LOG("encoderLeft %6.2f, encoderRight %6.2f\r\n", encoderLeft.velocity(), encoderRight.velocity());
+        LOG("encoderLeft %6.2fmm/s, encoderRight %6.2fmm/s\r\n", encoderLeft.velocity(), encoderRight.velocity());
+        LOG("encoderLeft %6.2fmm, encoderRight %6.2fmm\r\n", encoderLeft.distance(), encoderRight.distance());
         ledBar16.set(1<<(i&0xf));
         i++;
     }
