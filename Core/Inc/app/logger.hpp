bf99f92cef64f57cd9aee0aff2ef66cee5af850f
akiaki96 <akinabe1031@gmail.com>
2026-08-21 02:40:41 +0900

    logger.cppを実装

--- Core/Inc/app/logger.hpp
+++ Core/Inc/app/logger.hpp
@@ -2,6 +2,50 @@
 
 #include <cstdint>
 
+enum class LoggerState {
+    Idle,
+    Recording,
+    Stopped,
+};
+
+class Logger {
+public:
+    struct Filed {
+        const char* name;
+        const float* value;
+    };
+    static constexpr uint32_t MAX_FIELDS = 16;
+    static constexpr uint32_t MAX_BUFFER_SIZE = 8 * 3000;
+
+    bool add(const char* name, const float* value);
+
+    void start(void);
+    void stop(void);
+    void clear(void);
+
+    void sample(void);
+
+    void dump(void);
+    uint32_t dataSize(void) const;
+    uint32_t sampleCount(void) const;
+    uint32_t fieldCount(void) const;
+
+    bool isRecording(void) const;
+    bool isFull(void) const;
+
+private:
+    Filed fields_[MAX_FIELDS];
+    uint32_t fieldCount_ = 0;
+    float buffer_[MAX_BUFFER_SIZE];
+    uint32_t sampleCount_ = 0;
+    uint32_t maxSamples_ = MAX_BUFFER_SIZE;
+
+    bool isRecording_ = false;
+    bool isFull_ = false;
+
+    LoggerState state_ = LoggerState::Idle;
+};
+
 struct LogRecord {
     float time;
 
@@ -16,7 +60,7 @@ struct LogRecord {
     float gyroZ;
 };
 
-class Logger {
+class OldLogger {
 public:
     void start();
     void stop();
--- Core/Src/app/logger.cpp
+++ Core/Src/app/logger.cpp
@@ -2,18 +2,106 @@
 #include "device/device_instance.hpp"
 #include "common/debug.hpp"
 
-void Logger::start() {
+bool Logger::add(const char* name, const float* value) {
+    if (fieldCount_ >= MAX_FIELDS) {
+        return false;
+    }
+
+    if (name == nullptr || value == nullptr) {
+        return false;
+    }
+
+    fields_[fieldCount_] = {
+        .name = name,
+        .value = value,
+    };
+    ++fieldCount_;
+    maxSamples_ = MAX_BUFFER_SIZE / fieldCount_;
+
+    return true;
+}
+
+void Logger::start(void) {
+    sampleCount_ = 0;
+    state_ = LoggerState::Recording;
+}
+
+void Logger::stop(void) {
+    state_ = LoggerState::Stopped;
+}
+
+void Logger::clear(void) {
+    state_ = LoggerState::Stopped;
+    sampleCount_ = 0;
+}
+
+void Logger::sample(void) {
+    if (state_ != LoggerState::Recording) {
+        return;
+    }
+
+    if (sampleCount_ >= maxSamples_) {
+        state_ = LoggerState::Stopped;
+        return;
+    }
+
+    for (uint32_t i = 0; i < fieldCount_; ++i) {
+        buffer_[sampleCount_ * fieldCount_ + i] = *fields_[i].value;
+    }
+    ++sampleCount_;
+}
+
+void Logger::dump() {
+    printf("BIN_START\r\n");
+    printf("SIZE:%u\r\n", static_cast<uint32_t>(dataSize()));
+    for (uint32_t i = 0; i < fieldCount_; ++i) {
+        if (i > 0) {
+            printf(",");
+        }
+        printf("%s", fields_[i].name);
+    }
+    printf("\r\n");
+
+    uart_write(
+        reinterpret_cast<const uint8_t*>(buffer_),
+        dataSize()
+    );
+
+    printf("BIN_END\r\n");
+}
+
+uint32_t Logger::dataSize(void) const {
+    return sampleCount_ * fieldCount_ * sizeof(float);
+}
+
+uint32_t Logger::sampleCount(void) const {
+    return  sampleCount_;
+}
+
+uint32_t Logger::fieldCount(void) const {
+    return  fieldCount_;
+}
+
+bool Logger::isRecording(void) const {
+    return isRecording_;
+}
+
+bool Logger::isFull(void) const {
+    return isFull_;
+}
+
+void OldLogger::start() {
     recording_ = true;
     writeIndex_ = 0;
     timestamp_ms_ = 0;
     full_ = false;
 }
 
-void Logger::stop() {
+void OldLogger::stop() {
     recording_ = false;
 }
 
-void Logger::update() {
+void OldLogger::update() {
     timestamp_ms_ += 1;
     if (!recording_ || full_) {
         return;
@@ -36,7 +124,7 @@ void Logger::update() {
     }
 }
 
-void Logger::dump() {
+void OldLogger::dump() {
     printf("BIN_START\r\n");
     printf("SIZE:%d\r\n", sizeof(LogRecord) * writeIndex_);
     printf("time,battery,dutyL,dutyR,encoderL,encoderR,gyroZ\r\n");
