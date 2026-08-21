d6ad4e73b47d5ee6dc4bc4c5abe21046edbe2bb8
akiaki96 <akinabe1031@gmail.com>
2026-08-21 23:45:08 +0900

    fix: tree

--- Core/Src/menu/menuNode.cpp
+++ Core/Src/menu/menuNode.cpp
@@ -13,7 +13,7 @@ MenuNode::MenuNode(const char* name, MenuAction onSelected, MenuAction onEnter)
 
 void MenuNode::tree(uint8_t indent) const {
     for (uint8_t i = 0; i < indent; ++i) {
-        LOG("  \r");
+        LOG("  ");
     }
     LOG("- %1d:%s\r\n", childCount_, name_);
     for (uint8_t i = 0; i < childCount_; ++i) {
